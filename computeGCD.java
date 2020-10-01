8329a0baba358a1c5cf7f96b85c0e91a3a09dc8f
Tridip Rong <59462773+TridipRong@users.noreply.github.com>
2020-10-01 21:29:11 +0530

    Update computeGCD.java

--- computeGCD.java
+++ computeGCD.java
@@ -1,29 +1,26 @@
-
 import java.io.*;
 import java.util.*;
 
-public class computeGCD {
-
+public class Solution {
+    
     static int gcd(int a, int b)
-    {
-        if(b == 0)
-        {
-            return a;
-        }
-        return gcd(b, a % b);
+{
+      if(b == 0)
+      {
+        return a;
+      }
+      return gcd(b, a % b);
     }
 
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-        computeGCD obj = new computeGCD();
+        Solution obj = new Solution();
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Number 1: ");
         int n1 = sc.nextInt();
-        System.out.println("Enter Number 2:");
         int n2 = sc.nextInt();
         int result = obj.gcd(n1,n2);
-        System.out.println("GCD: "+result);
-
-
+        System.out.println(result);
+        
+        
     }
 }
